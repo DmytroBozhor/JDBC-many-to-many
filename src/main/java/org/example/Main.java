1d1c194bebc06d98f196b73b5e8b322abec0e9d6
DmytroBozhor <dimaastop1@gmail.com>
2023-09-27 19:50:35 +0200

    hibernate spring configurateion & dao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,50 +1,21 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.example.dao.ActorDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Component
 public class Main {
     public static void main(String[] args) {
 
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext("org.example");
 
-        Session session = sessionFactory.getCurrentSession();
+        ActorDao actorDao = (ActorDao) applicationContext.getBean("actorDao");
 
-        try (sessionFactory) {
-            session.beginTransaction();
+        System.out.println(actorDao.get(1));
 
-            Actor actor = session.get(Actor.class, 1);
-            System.out.println(actor);
-            List<Movie> movieList = actor.getMovies();
-            movieList.forEach(System.out::println);
+        System.out.println(actorDao.getALl());
 
-            Movie movie = session.get(Movie.class, 3);
-            System.out.println(movie);
-            List<Actor> actorList = movie.getActors();
-            actorList.forEach(System.out::println);
-
-            Actor actor1 = new Actor("Bon Trafinn", 31);
-            Movie movie1 = new Movie("Cliff", 2021);
-
-            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie1)));
-            movie1.setActors(new ArrayList<>(Collections.singletonList(actor1)));
-
-            session.save(actor1);
-            session.save(movie1);
-
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
     }
 }
